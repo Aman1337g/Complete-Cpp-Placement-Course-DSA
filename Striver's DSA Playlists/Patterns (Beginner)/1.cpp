b49a9db95e6c4144383e6e6eab9039bf8880d7e8
Aman Kumar Gupta <aman65144kumar@gmail.com>
2023-12-13 17:30:36 +0530

    Solving Striver Pattern(Beginner) Playlist

---
+++ Striver's DSA Playlists/Patterns (Beginner)/1.cpp
@@ -0,0 +1,31 @@
+// Q Link - https://www.codingninjas.com/studio/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+/*
+OUTPUT
+
+5
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
